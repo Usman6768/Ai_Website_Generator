3a787a57fd449c951114c8570d42980ccbbf6ce4
Usman pervez <usmanpervez.work@gmail.com>
2025-12-10 10:29:00 +0500

    chore: remove unused fireEvent import from App.test.jsx

--- __tests__/App.test.jsx
+++ __tests__/App.test.jsx
@@ -1,5 +1,5 @@
 // __tests__/App.test.jsx
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import App from "../src/App";
 import React from "react";
